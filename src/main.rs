b62e6d0f9a6fc59cbfa7ae8d036bb3ece295b1ed
wlingze <lingze.wl@gmail.com>
2022-12-25 15:00:51 +0800

    perfect main function & echo.bf code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use std::{env, process::exit};
+
+use vm::VM;
+
 pub mod jit;
 pub mod tokenizer;
 pub mod vm;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("usage bfjit <file.bf>");
+        exit(1);
+    }
+
+    let filepath = &args[1];
+    VM::new_from_file(filepath).expect("build vm failed").run();
 }
